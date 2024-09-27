3ad6f0520764ddc8ce4328b35b25fcafb416beb3
UbiquityMVP <admin@ubiquityvx.com>
2024-09-27 15:42:47 -0700

    Create models.ts

---
+++ src/utils/cdn-links/models.ts
@@ -0,0 +1,9 @@
+export const maleModel1 = 'https://cdn.glitch.me/155b1488-cef3-43d5-92c7-da25735e6c95/ManAvatar_Hair.vrm?v=1724102964867';
+
+export const femaleModel1 = 'https://cdn.glitch.me/155b1488-cef3-43d5-92c7-da25735e6c95/WomanAvatar%20(1).vrm?v=1725564776409';
+
+export const officeModel = 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/IndoorOfficeModel.glb?v=1724103050803';
+
+export const groundPlane = 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/GroundPlane.gltf?v=1724103109351';
+
+export const treeModel = 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/OutdoorTreeModel.glb?v=1724103170386';
