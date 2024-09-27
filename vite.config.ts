6d56005d80cd5d068079c9c676566eb7f2f79526
UbiquityMVP <admin@ubiquityvx.com>
2024-09-27 15:33:22 -0700

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { mediapipe } from 'vite-plugin-mediapipe'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    mediapipe({
+      'holistic.js': ['VERSION', 'POSE_CONNECTIONS', 'HAND_CONNECTIONS', 'FACEMESH_TESSELATION', 'Holistic']
+    }),
+  ],
+})
