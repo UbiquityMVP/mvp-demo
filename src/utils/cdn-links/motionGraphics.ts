5b1bb3c2fbd848e377fe782873015b669e2d00c5
UbiquityMVP <admin@ubiquityvx.com>
2024-09-27 15:43:06 -0700

    Create motionGraphics.ts

---
+++ src/utils/cdn-links/motionGraphics.ts
@@ -0,0 +1,31 @@
+// BASE BACKGROUND ANIMATION - THE ONE THAT LOOKS LIKE MOCAP DATA
+export const baseBGAnimation = 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/BaseBGAnimation.mp4?v=1724098592130';
+
+export type H2PlayCard = {
+  id: number;
+  graphic: string;
+}
+// HOW TO PLAY SCREEN
+export const h2Play = [
+  {
+    id: 0, // lateral raise graphic
+    graphic: 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/howToPlayAbduction.webm?v=1725386211408'
+  },
+  {
+    id: 1, // frontal raise graphic
+    graphic: 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/howToPlayFlexion.webm?v=1725386216617'
+  },
+  {
+    id: 2, // cross body graphic
+    graphic: 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/howToPlayAdduction.webm?v=1725386214468'
+  }
+];
+
+// COUNTDOWN SCREEN
+export const countdownScreen = 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/countdownNoAudio.webm?v=1724099159345';
+
+// LOADING SCREEN
+export const loadingSplashAnimation = 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/BottomLoadingSplashAnimation.webm?v=1724099280553';
+
+// SLIDING INFO
+export const slidingInfoBG = 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/SlidingInfoBGAnimation.webm?v=1724099325758';
